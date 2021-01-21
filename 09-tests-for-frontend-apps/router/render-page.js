2145d5a7308a009bdeef4cfc568bb577afccca55
ildar <ildar.latipov@gmail.com>
2021-01-21 18:37:24 +0100

    solutions for project

---
+++ 09-tests-for-frontend-apps/router/render-page.js
@@ -0,0 +1,10 @@
+export default async function(path, match) {
+  const { default: Page } = await import(`./10-webpack-part-1/pages`);
+  const page = new Page(match);
+  const contentNode = document.querySelector('#content');
+
+  contentNode.innerHTML = '';
+  contentNode.append(page.element);
+
+  return page;
+}
